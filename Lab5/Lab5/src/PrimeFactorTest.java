20292d3b0c5f4e48a7af5fcc11e2466eb94c5608
Matthew Klaasse <klaassmj@rose-hulman.edu>
2011-03-28 15:46:47 -0400

    added Karl's test for prime(1)

---
+++ Lab5/Lab5/src/PrimeFactorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PrimeFactorTest {
+
+	@Test
+	public void testJUnit(){
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testPrimeOne(){
+		assertNull(PrimeFactor.prime(1));
+	}
+}
