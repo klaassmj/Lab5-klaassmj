0f9a271d016c9c8327a25b270bc58fc39cf2aa69
Matthew Klaasse <klaassmj@rose-hulman.edu>
2011-03-28 16:02:02 -0400

    added Karl's solution for prime(2)

--- Lab5/Lab5/src/PrimeFactor.java
+++ Lab5/Lab5/src/PrimeFactor.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 
 public class PrimeFactor {
 
-	public static Object prime(int i) {
+	public static ArrayList<Integer> prime(int i) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		if (i > 1){
+			temp.add(i);
+			return temp;
+		}
 		return null;
 	}
 
