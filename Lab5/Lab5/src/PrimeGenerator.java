80084b84a2566d71e1d6efc77f92a07702aafe77
Matthew Klaasse <klaassmj@rose-hulman.edu>
2011-03-28 20:25:09 -0400

    added Karl GeneratePrimes(3)

--- Lab5/Lab5/src/PrimeGenerator.java
+++ Lab5/Lab5/src/PrimeGenerator.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
 
-
 public class PrimeGenerator {
-	
-	public static ArrayList<Integer> generatePrimes(int i){
-		return new ArrayList<Integer>();
+
+	public static ArrayList<Integer> generatePrimes(int i) {
+		if (i <= 2) {
+			return new ArrayList<Integer>();
+		}
+		else {
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			
+			if (i == 3){
+				temp.add(2);
+			}
+			return temp;
+		}
 	}
 
 }
